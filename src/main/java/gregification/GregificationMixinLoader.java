dc903b69e2a9424720d08f3541f101ec6d9fa33b
Arisu Desu <arisudesu@yandex.ru>
2023-09-16 21:06:34 +0300

    Remove tfc mixins

--- src/main/java/gregification/GregificationMixinLoader.java
+++ src/main/java/gregification/GregificationMixinLoader.java
@@ -6,8 +6,5 @@ import zone.rong.mixinbooter.MixinLoader;
 @MixinLoader
 public class GregificationMixinLoader {
 
-    public GregificationMixinLoader() {
-        // Insert Mixins.addConfiguration(...) here
-        Mixins.addConfiguration("mixins.tfc.json");
-    }
+    public GregificationMixinLoader() { }
 }
